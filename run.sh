3d6f40e7977fde5abdbac4944849182d8b026f6d
jrtrevino <j.trevino95@icloud.com>
2021-12-01 13:52:46 -0800

    added repo cloning in shell

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
+# prepare directory
+rm -rf web
+# clone index page
+git clone https://bitbucket.org/bjgiller/evt-tech-challenge/raw/master/evt-web.html web/
+# rename 
+mv web/*.html web/index.html
+# remove git info
+rm -rf web/.git*
+# build and run
 docker build -t my-web-server-image .
 docker run -p 4443:4443 my-web-server-image
