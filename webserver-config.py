467e2edbd89727c8f2b046bdf60013af94ddc74b
jrtrevino <j.trevino95@icloud.com>
2021-11-30 16:17:09 -0800

    commented

--- webserver-config.py
+++ webserver-config.py
@@ -1,7 +1,13 @@
-import http.server, ssl
+import http.server
+import ssl
 import argparse
+
 # directory containing index.html file
 DIRECTORY = "web"
+
+# Manages GET requests on index.html
+# Uses the SimpleHTTPRequestHandler and the defined directory above
+# to manage the default directory.
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
@@ -9,20 +15,21 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
 
-# Uses http server to serve index.html file on host:port
+# Uses http server to serve the index.html file on host:port.
 def serve(host, port):
     server_address = (host, port)
     httpd = http.server.HTTPServer(server_address, Handler)
     httpd.socket = ssl.wrap_socket(httpd.socket,
-                                server_side=True,
-                                certfile='./server/localhost.pem',
-                                ssl_version=ssl.PROTOCOL_TLS)
+                                   server_side=True,
+                                   certfile='./server/localhost.pem',
+                                   ssl_version=ssl.PROTOCOL_TLS)
     print(f"Server {host} running on port: {port}")
     httpd.serve_forever()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('port', type=int, help="Port to run server on") 
-    parser.add_argument('host', type=str, help="host name") 
+    parser.add_argument('port', type=int, help="Port to run server on")
+    parser.add_argument('host', type=str, help="host name")
     args = parser.parse_args()
     serve(args.host, args.port)
